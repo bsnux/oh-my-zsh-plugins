abc07eb260a5584ea164e81754306a972a23e7e2
Arturo Fernandez <arturo.fernandez@ff0000.com>
2013-10-14 19:11:19 +0200

    Initial commit

---
+++ django/django.plugin.zsh
@@ -0,0 +1,21 @@
+function dj() {
+    if [ -f bin/django ]; then
+        bin/django $* # Buildout
+    elif [ -f src/manage.py ]; then
+        python src/manage.py # src/ folder
+    elif [ -f project/manage.py ]; then
+        python project/manage.py $* # django default
+    elif [ -f manage.py ]; then
+        python manage.py $* # plain
+    else
+        echo "dj: $fg[red]Error:$reset_color Could not find manage.py or buildout"
+    fi
+}
+
+alias djr='dj runserver --traceback'
+# Next alias requires 'django_extensions' app. in your project
+alias djs='dj shell_plus'
+alias djd='dj dbshell'
+alias djm='dj migrate'
+alias djsync='dj syncdb'
+alias djclean='find . -name \*.pyc | xargs rm'
